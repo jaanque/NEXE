4150296a3f3e0ff7ccbdf2b998ff145890425431
Jan <jan@ilerda.com>
2026-04-24 19:54:04 +0200

    Initial Commit

---
+++ NEXE/NEXEApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEXEApp.swift
+//  NEXE
+//
+//  Created by Jan Queralt Posino on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct NEXEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
